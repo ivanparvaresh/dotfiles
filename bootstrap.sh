cf723155eca3c43ab170d6f117ca0dc0c6775601
ivan <javadparvaresh@gmail.com>
2018-09-12 15:00:43 +0300

    added .aliasses bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
 git pull origin ubuntu;
-source ~/.bash_profile;
+
+
+rsync --exclude ".git/" \
+    --exclude ".DS_Store" \
+    --exclude ".osx" \
+    --exclude "bootstrap.sh" \
+    --exclude "README.md" \
+    --exclude "LICENSE-MIT.txt" \
+    -avh --no-perms . ~;
